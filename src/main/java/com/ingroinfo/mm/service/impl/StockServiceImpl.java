1d7ed28df0792eaee9b3fa7ba609f97b2d07b514
Panneerselvam <Panneer867@live.com>
2023-03-29 21:54:12 +0530

    update

--- src/main/java/com/ingroinfo/mm/service/impl/StockServiceImpl.java
+++ src/main/java/com/ingroinfo/mm/service/impl/StockServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
