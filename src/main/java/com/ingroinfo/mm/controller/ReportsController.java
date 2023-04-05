577289c799751f6f8865444739f28d6eb1493555
Panneerselvam <30413680+Panneer867@users.noreply.github.com>
2023-04-05 18:31:44 +0530

    update

--- src/main/java/com/ingroinfo/mm/controller/ReportsController.java
+++ src/main/java/com/ingroinfo/mm/controller/ReportsController.java
@@ -5,8 +5,9 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.ingroinfo.mm.dao.AssetRepository;
 import com.ingroinfo.mm.entity.Assets;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -16,20 +17,21 @@ import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-@RestController
+@Controller
+@RequestMapping("/reports")
 public class ReportsController {
 
 	@Autowired
 	AssetRepository assetRepository;
 
-	@GetMapping("/report")
+	@GetMapping("/assets")
 	public void generateReport(HttpServletResponse response) throws Exception {
 
 		List<Assets> assets = assetRepository.findAll();
 
 		// Compile the .jrxml file to a .jasper file
 		JasperReport jasperReport = JasperCompileManager
-				.compileReport(getClass().getResourceAsStream("/reports/Assets_Report.jrxml"));
+				.compileReport(getClass().getResourceAsStream("/reports/Report.jrxml"));
 
 		// Generate the report data
 		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(assets);
