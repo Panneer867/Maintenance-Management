082314d3e9211417970359d34d13b2ed12a5eaa1
Panneerselvam <30413680+Panneer867@users.noreply.github.com>
2023-04-10 09:40:14 +0530

    Update AssetsRepository.java

--- src/main/java/com/ingroinfo/mm/dao/AssetsRepository.java
+++ src/main/java/com/ingroinfo/mm/dao/AssetsRepository.java
@@ -3,7 +3,7 @@ package com.ingroinfo.mm.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ingroinfo.mm.entity.Assets;
 
-public interface AssetRepository extends JpaRepository<Assets,Long>{
+public interface AssetsRepository extends JpaRepository<Assets,Long>{
 	
 
 }
